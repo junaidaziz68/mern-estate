fc0889c9328b49907bf335f00712469fe953138c
junaidaziz68 <junaidaziz68@outlook.com>
2023-10-17 19:35:08 +0100

    Created sign up route

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import userRouter from './routes/user.route.js';
+import authRouter from './routes/auth.route.js'
 import dotenv from 'dotenv';
 
 dotenv.config();
@@ -14,9 +15,12 @@ mongoose.connect(process.env.db).then(()=>{
 })
 // main config to setup server
 const app = express()
+// allows us to view json request from server 
+app.use(express.json());
 const port = 3000
 
 app.use('/server/user',userRouter);
+app.use('/server/auth',authRouter)
 
 app.listen(port,()=>{
 console.log(`App running on port ${port}`)
--- server/models/user.model.js
+++ server/models/user.model.js
@@ -23,7 +23,7 @@ const userSchema =  new  mongoose.Schema({
 // creating models using mongoose.model method
 
 
-const User = mongoose.Model('User',userSchema)
+const User = mongoose.model('User',userSchema)
 
 
 export default User;
