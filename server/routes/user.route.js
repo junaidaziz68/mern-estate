85f2e05afa9c029ff916df777a282c5ee3dd298a
junaidaziz68 <junaidaziz68@outlook.com>
2023-10-17 15:37:33 +0100

    created test api route

--- server/routes/user.route.js
+++ server/routes/user.route.js
@@ -1,16 +1,13 @@
 import express from 'express';
+import { test } from '../controllers/user.controller.js';
 
 const router=express.Router();
 
 
+// creating routes 
 
 
-
-router.get('/test',(req,res)=>{
-    res.json({
-        message:"hello mr t"
-    })
-})
+router.get('/test',test)
 
 
 export default router;
